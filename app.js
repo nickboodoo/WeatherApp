493c493d7084dfaf9060e4929c8be209a53521d5
nickboodoo <boodoo@temple.edu>
2023-08-09 00:24:25 -0400

    modified: app.js

--- app.js
+++ app.js
@@ -26,3 +26,45 @@ if('geolocation' in navigator){
     notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p>Browser does not support geolocation</p>";
 }
+
+// SET USER POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude,longitude);
+}
+
+// SHOW ERROR
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`
+}
+    
+// GET WEATHER FROM API
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    // console.log(api);
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].icon;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        displayWeather();
+    });
+
+}
+
+// DISPLAY WEATHER TO UI
+
+
