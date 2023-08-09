f533059780fd5ede5985dadd684f115a18dd9d1a
nickboodoo <boodoo@temple.edu>
2023-08-09 00:44:23 -0400

    finished: app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const iconElement = document.querySelector(".weather-icon")
 const tempElement = document.querySelector(".temperature-value p")
 const descElement = document.querySelector(".temperature-description p")
-const loccationElement = document.querySelector(".location p")
+const locationElement = document.querySelector(".location p")
 const notificationElement = document.querySelector(".notification")
 
 // APP DATA
@@ -54,7 +54,7 @@ function getWeather(latitude, longitude){
     })
     .then(function(data){
         weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-        weather.description = data.weather[0].icon;
+        weather.description = data.weather[0].description;
         weather.iconId = data.weather[0].icon;
         weather.city = data.name;
         weather.country = data.sys.country;
@@ -66,5 +66,34 @@ function getWeather(latitude, longitude){
 }
 
 // DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C<span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
 
+// TEMPERATURE CONVERSION
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+
+}
+
+// CHANGE TEMPERATURE UNIT ON USER CLICK
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined)
+        return;
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F<span>`;
+        weather.temperature.unit = "fahrenheit";
+    
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C<span>`;
+        weather.temperature.unit = "celsius";
+
+    }
+})
 
